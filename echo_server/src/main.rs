cd1834394f8fec5853059da2b49d871808412c29
CJ Cobb <charcobb@sas.upenn.edu>
2015-09-08 00:43:16 -0400

    some comments

--- echo_server/src/main.rs
+++ echo_server/src/main.rs
@@ -13,12 +13,19 @@ fn main() {
     for stream in listener.incoming() {
     	match stream {
     		Ok(stream) => {
+    			//this is not necessary, but just shows a pattern
     			thread::spawn(|| {
+    				//makes the stream mutable
     				let mut stream = stream;
+    				//a mutable buffer to write the bytes into
     				let mut buf : [u8; 128] = [0 ; 128]; 
+    				//returns number of bytes, which we dont care about
     				let _ = stream.read(&mut buf).unwrap();
+    				//bytes to string conversion
     				println!("message is {}",str::from_utf8(&buf).unwrap());
+    				//just used to show multithreading
     				thread::sleep_ms(5000);
+    				//note the b
     				stream.write(b"yea, I got your message").unwrap();
 
     			});
