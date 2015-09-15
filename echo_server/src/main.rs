501e5a95656b07004f4b200ed9a653382811693c
CJ Cobb <charcobb@sas.upenn.edu>
2015-09-15 14:48:15 -0400

    key can be sent over socket now

--- echo_server/src/main.rs
+++ echo_server/src/main.rs
@@ -8,61 +8,62 @@ use std::io::Read;
 use std::str;
 use std::thread;
 
+use std::string::String;
+
 use bson::Bson;
 use mongodb::{Client,ThreadedClient};
 use mongodb::db::ThreadedDatabase;
 
 
+//Send a key, receive all values associated with a given key in the mongo collection mydb.coll1 running on local machine
 fn main() {
     println!("Starting server");
 
-    let client = Client::connect("localhost",27017).ok().expect("Failed to initialize client");
-
-    let coll = client.db("mydb").collection("coll1");
-
-    let doc = doc ! {"key" => "foo"};
-    let doc2 = doc ! {"key" => "foobar", "value" => "fuguuhh"};
-
-    let mut cursor = coll.find(Some(doc.clone()), None).ok().expect("failed to execute find");
-        
-
-        //let item = cursor.next();
-
-
-    for item in cursor {
-        match item {
-            Ok(doc) => match doc.get("value") {
-                Some(&Bson::String(ref val)) => println!("Value is {}", val),
-                _ => println!("Error, improper field"),
-            },
-            Err(_) => println!("Error fetching document"),
-        }
-    }
-
-
-
     let listener = TcpListener::bind("127.0.0.1:9999").unwrap();
 
+    //this is just going to stay open forever, until we shut the server down
     for stream in listener.incoming() {
     	match stream {
     		Ok(stream) => {
     			//this is not necessary, but just shows a pattern
     			thread::spawn(|| {
+                    //should have these outside of thread
+                    let client = Client::connect("localhost",27017).ok().expect("Failed to initialize client");
+                    let coll = client.db("mydb").collection("coll1");
+
     				//makes the stream mutable
     				let mut stream = stream;
     				//a mutable buffer to write the bytes into
     				let mut buf : [u8; 128] = [0 ; 128]; 
-    				//returns number of bytes, which we dont care about
+    				//returns number of bytes, which we dont care about. but reads them into the buffer
     				let _ = stream.read(&mut buf).unwrap();
-    				//bytes to string conversion
-    				println!("message is {}",str::from_utf8(&buf).unwrap());
+    				//bytes to string conversion. This array of bytes has a bunch of zeroes though
+                    let lookup_str = str::from_utf8(&buf).unwrap();
+    				
+                    println!("message is {}, length of message is {}",&lookup_str[0..4], lookup_str.len());
 
+                    //need to throw out those zeroes
+                    let doc = doc ! {"key" => (&lookup_str[0..true_len(lookup_str)])};
 
+                    //note the clone, so we dont run into problems with the borrower
+                    let cursor = coll.find(Some(doc.clone()), None).ok().expect("failed to execute find");
 
-    				//just used to show multithreading
-    				thread::sleep_ms(5000);
-    				//note the b
-    				stream.write(b"yea, I got your message").unwrap();
+                    //get all the values associated with a given key
+                    let mut response_msg : String = "".to_string();
+                    for item in cursor {
+                        match item {
+                            Ok(doc) => match doc.get("value") {
+                                Some(&Bson::String(ref val)) => {
+                                    response_msg.push_str(val); 
+                                    response_msg.push_str(" ");},
+                                _ => println!("Error, improper field"),
+                            },
+                            Err(_) => println!("Error fetching document"),
+                        }
+                    }                    
+
+    				//send back the response
+    				stream.write(response_msg.as_bytes()).unwrap();
 
     			});
     		}
@@ -72,4 +73,18 @@ fn main() {
     	}
     }
     drop(listener);
+}
+
+//finds the index of the last non null byte in a string
+fn true_len(x : &str) -> usize {
+    let mut i = 0;
+    for c in x.as_bytes() {
+        if *c == 0 {
+            break;
+        }
+        else {
+            i = i + 1;
+        }
+    }
+    i
 }
--- test_client/src/main.rs
+++ test_client/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Hello, world!");
 
     let mut stream = TcpStream::connect("127.0.0.1:9999").unwrap();
-    let _ = stream.write(b"foo");
+    let _ = stream.write(b"key1");
     let mut buf : [u8; 128] = [0; 128];
     let _ = stream.read(&mut buf).unwrap();
     let foo = str::from_utf8(&buf).unwrap();
